162289a16f5a12ebb75e669fa0bb65aaa302d277
benjamin Staine <93956622+benjamin-Staine@users.noreply.github.com>
2022-10-23 16:31:17 -0500

    realizacion de los videos 18-20
    realizacion de los videos 18-20

--- src/com/tuempresa/facturacion/modelo/Pedido.java
+++ src/com/tuempresa/facturacion/modelo/Pedido.java
@@ -3,14 +3,17 @@ package com.tuempresa.facturacion.modelo;
 import java.time.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 import org.openxava.annotations.*;
+import org.openxava.util.*;
 
 import lombok.*;
 
 @Entity @Getter @Setter
 @View( extendsView="super.DEFAULT",
-members="diasEntregaEstimados," + 
+	   members=
+	   "diasEntregaEstimados, entregado, " +
 		"factura { factura } "
 )
 @View( name="SinClienteNiFactura", 
@@ -41,6 +44,23 @@ public class Pedido extends DocumentoComercial{
     @PrePersist @PreUpdate 
     private void recalcularDiasEntrega() {
         setDiasEntrega(getDiasEntregaEstimados());
+    }
+    
+    @Column(columnDefinition="BOOLEAN DEFAULT FALSE")
+    boolean entregado;
+    
+    @AssertTrue(message="pedido_debe_estar_entregado")
+    	private boolean isEntregadoParaEstarEnFactura() { 
+    	    return factura == null || isEntregado(); 
+    	}
+    
+    @PreRemove
+    private void validarPreBorrar() {
+        if (factura != null) { 
+            throw new javax.validation.ValidationException( 
+                XavaResources.getString( 
+                    "no_puede_borrar_pedido_con_factura"));
+        }
     }
 	
 }
